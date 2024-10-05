c012367bfbbea1e92f61d92515b09f4ebeedc533
DAandDA2004 <vd54594@gmail.com>
2024-10-05 09:02:42 +0900

    first commit

---
+++ app/src/main/java/com/example/weatherappcursey/DayItem.kt
@@ -0,0 +1,11 @@
+package com.example.weatherappcursey
+
+data class DayItem (
+    val city: String,
+    val time: String,
+    val condition: String,
+    val imageUrl: String,
+    val currentTemp: String,
+    val maxTemp: String,
+    val hours: String
+)
