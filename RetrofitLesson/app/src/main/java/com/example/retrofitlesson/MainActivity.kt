443af4c61f13afb15dd3d252bf6139a60ef815c5
DAandDA2004 <vd54594@gmail.com>
2024-10-09 14:56:36 +0900

    Добавил папку RetrofitLesson

---
+++ RetrofitLesson/app/src/main/java/com/example/retrofitlesson/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.retrofitlesson
+
+import android.os.Bundle
+import android.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.retrofitlesson.adapter.ProductAdapter
+import com.example.retrofitlesson.databinding.ActivityMainBinding
+import com.example.retrofitlesson.retrofit.AuthRequest
+import com.example.retrofitlesson.retrofit.MainApi
+import com.example.retrofitlesson.retrofit.User
+import com.squareup.picasso.Picasso
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var adapter: ProductAdapter
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        adapter = ProductAdapter()
+        binding.rcView.layoutManager = LinearLayoutManager(this)
+        binding.rcView.adapter = adapter
+
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://dummyjson.com").client(client)
+            .addConverterFactory(GsonConverterFactory.create()).build()
+        val mainApi = retrofit.create(MainApi::class.java)
+
+        var user: User? = null
+
+        binding.sv.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                CoroutineScope(Dispatchers.IO).launch {
+                    val response = newText?.let { mainApi.getProductByNameAuth(user?.token ?: "", it) }
+                    runOnUiThread {
+                        binding.apply {
+                            adapter.submitList(response?.products)
+                        }
+                    }
+                }
+                return true
+            }
+        })
+    }
+}
